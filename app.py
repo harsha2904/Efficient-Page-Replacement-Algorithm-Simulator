cfda7b310fbd954baef9a321cbcce9a77fb5d027
THELITEESHREDDY <liteeshreddypotlapalli.3605@gmail.com>
2025-03-23 16:13:42 +0530

    added function FIFO

--- app.py
+++ app.py
@@ -2,3 +2,16 @@ import streamlit as st
 
 st.title("Hello, Streamlit!")
 st.write("This is my first Streamlit app.")
+
+def fifo(self):
+        page_faults = 0
+        memory = []
+        for page in self.reference_string:
+            if page not in memory:
+                if len(memory) < self.frames:
+                    memory.append(page)
+                else:
+                    memory.pop(0)
+                    memory.append(page)
+                page_faults += 1
+        return page_faults
