2d927674b34abdc9caed66a1187f3c34cb4b2f2f
THELITEESHREDDY <liteeshreddypotlapalli.3605@gmail.com>
2025-03-24 21:38:14 +0530

    Added LRU functionality

--- app.py
+++ app.py
@@ -23,3 +23,22 @@ frames = st.number_input("Enter Number of Frames", min_value=1, value=3, step=1)
 algorithm = st.selectbox("Choose Algorithm", ["FIFO", "LRU", "Optimal"])
 
 st.button("Run Simulation")
+
+#added lru functionality
+#this function used to store pages that are recently used with min time 
+#when there is no enough memory to store the pages, we'll see less recently used page and replace it with current page
+def lru(self):
+        page_faults = 0
+        memory = []
+        recent_usage = {}#stores  time, to check least recently used page 
+        for i, page in enumerate(self.reference_string): #list of pages that algo will enumerate
+            if page not in memory:
+                if len(memory) < self.frames:
+                    memory.append(page)
+                else:
+                    lru_page = min(memory, key=lambda p: recent_usage.get(p, float('-inf')))#gets most recently used page
+                    memory.remove(lru_page)
+                    memory.append(page)
+                page_faults += 1
+            recent_usage[page] = i
+        return page_faults
